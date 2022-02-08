01c3cf81957076702bd772bf509a944e72327e7a
sergiogval <sergioval@gmail.com>
2022-02-08 16:15:31 -0600

    Add style

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-import './path'
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import * as path from "path";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import * as path from 'path';
 
 module.exports = {
-	entry: './src/index.js',
-	mode: 'development',
-	devServer: {
-		static: './dist',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader','css-loader'],
-			},
-		],
-	},
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
