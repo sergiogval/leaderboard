f73297f6f4976849c2904bc8297cda195fdd254c
sergiogval <sergioval@gmail.com>
2022-02-08 16:02:56 -0600

    Add style

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+import './path'
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import * as path from "path";
 
 module.exports = {
 	entry: './src/index.js',
