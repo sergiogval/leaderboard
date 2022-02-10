d0a6c0540339834c92535e4c9043cf5ff0271811
Juan Vasquez <juan@logicalbricks.com>
2022-02-09 21:03:30 -0600

    chore: fetch data from games API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './style.css';
+import "./style.css";
+
+const BASE_URL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api";
+
+function fetchGame() {
+  const url = `${BASE_URL}/games/Zl4d7IVkemOTTVg2fUdz/scores`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchGame();
+});
